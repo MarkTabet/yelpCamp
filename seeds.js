4bcf6bcc50207d5af60cb2f708a32d2be6c0e56f
Mark Tabet <marktabet@MacBook-Pro.local>
2020-04-18 00:25:58 +0100

    Refactor seed.js

--- seeds.js
+++ seeds.js
@@ -11,62 +11,119 @@ db.once('open', function() {
 var Campground = require( "./models/campground" );
 var Comment = require( "./models/comment" );
 
-var seedData = [
+var seeds = [
     {
-        name: "Cloud's Rest", 
-        image: "https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069__340.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        name: "Downton Abbey",
+        price: "399.99",
+        image: "https://res.cloudinary.com/mark2code/image/upload/v1587033196/dlfqqekmrh51chhhl0jj.jpg",
+        image_id: "dlfqqekmrh51chhhl0jj",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        location: "Highclere Park, Highclere, Newbury RG20 9RN, UK",
+        lat: 51.3265901,
+        lng: -1.360666,
+        createdAt: {
+            type: "2020-04-16T10:33:15.041Z",
+            default: Date.now
+        },
+        author: {
+            id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            },
+            username: "Mark"
+        }    
     },
     {
-        name: "Desert Mesa", 
-        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-    },
-    {
-        name: "Canyon Floor", 
-        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-    }
-]
-
-
-
-function seedDB(){
-    // remove all campgrounds
-    Campground.remove( {}, function( err ){
-        if( err ){
-            console.log( err );
-        } else {
-            console.log( "database cleared" );
-            // add some campgrounds
-            // seedData.forEach( function( seed ){
-            //     Campground.create( seed, function( err, campground ){
-            //         if( err ){
-            //             console.log( err );
-            //         } else {
-            //             console.log( "campground added" );
-            //             Comment.create( 
-            //                 {
-            //                     text: "This place is great, but I wish there was WiFi",
-            //                     author: "Homer" 
-            //                 }, function( err, comment ){
-            //                     if( err ){
-            //                         console.log( err );
-            //                     } else {
-            //                         campground.comments.push( comment );
-            //                         campground.save();
-            //                         console.log( "comment added" );
-            //                     }            
-            //                 });
-            //         }
-            //     });
-            // });
+        name: "Ain Zhalta",
+        price: "14.99",
+        image: "https://res.cloudinary.com/mark2code/image/upload/v1587056884/diilkogop6myaleqnxhs.webp",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        location: "Ain Zhalta, Lebanon",
+        lat: 33.7413374,
+        lng: 35.7004322,
+        createdAt: {
+            type: "2020-04-16T10:33:15.041Z",
+            default: Date.now
+        },
+        author: {
+            id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            },
+            username: "Mark"
         }
-    });
-    
-    
-    // add some comments
+    },
+    {
+        name: "Clouds Rest",
+        price: "24.99",
+        image: "https://res.cloudinary.com/mark2code/image/upload/v1587057013/katxfvrp45zrijjuzqvp.webp",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        location: "Himalayas",
+        lat: 28.5983159,
+        lng: 83.9310623,
+        createdAt: {
+            type: "2020-04-16T10:33:15.041Z",
+            default: Date.now
+        },
+        author: {
+            id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            },
+            username: "Mark"
+        }
+    },
+    {
+        name: "Highams Park", 
+        price: "1.99",
+        image: "https://res.cloudinary.com/mark2code/image/upload/v1587064150/ec82egnetiefuqqbxhlp.webp",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        location: "Himalayas",
+        lat: 51.6083754,
+        lng: 0.0014712,
+        createdAt: {
+            type: "2020-04-16T10:33:15.041Z",
+            default: Date.now
+        },
+        author: {
+            id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            },
+            username: "Mark"
+        }
+    }
+];
+
+
+async function seedDB(){
+    // remove all comments & campgrounds
+    try {
+        await Campground.remove({});
+        console.log( "campgrounds cleared" );
+        await Comment.remove({});
+        console.log( "comments cleared" );
+        
+        // add fresh comments & campgrounds
+        for( const seed of seeds ){
+            let campground = await Campground.create( seed );
+            console.log( "campground created" );
+            let comment = await Comment.create(
+                {
+                    text: "This place is great, but I wish there was WiFi",
+                    author: "Colt" 
+                }
+            );
+            console.log( "comment created" );
+            campground.comments.push( comment );
+            campground.save();
+            console.log( "comment added to campground" );
+        }
+    } catch( err ) {
+        console.log( err );
+    }
 }
+
 
 module.exports = seedDB;
 
